85ed5c09f62b5cc705d594dcc6ce24d2b4eae567
hpsleuth7 <murphy.c.austin@gmail.com>
2020-05-07 16:41:45 -0400

    add function to create landlord chart w ggplot

--- hdc/watchlist_analysis.R
+++ hdc/watchlist_analysis.R
@@ -1,6 +1,6 @@
 watchlist_agg <- function(watchlist=NULL,boro=FALSE) {
   # Usage: takes dataframe of landlord watchlist, optional borough flag
-	"Brooklyn" / "Queens" / "Manhattan" / "Bronx" / "Staten Island"
+  #	"Brooklyn" / "Queens" / "Manhattan" / "Bronx" / "Staten Island"
   #    returns new dataframe aggregated by landlord with sum and avg of viols+bldgs
 
   library(dplyr)
@@ -30,7 +30,7 @@ watchlist_agg <- function(watchlist=NULL,boro=FALSE) {
 
 }
 
-landlord_top_ten <- function(landlords=NULL) {
+get_top_ten <- function(landlords=NULL) {
   # Usage: takes dataframe of watchlist records aggregated by landlord
   #	returns ordered dataframe of ten landlords with most violations
 
@@ -45,6 +45,41 @@ landlord_top_ten <- function(landlords=NULL) {
 return(top_ten)
 
 }
+
+plot_landlord_viols <- function(landlords=NULL) {
+  # Usage: takes dataframe of landlords with total_viols column
+  # Plots column chart, saves as PDF in working directory, and returns plot object
+  
+  library(tidyverse)
+  ###
+  
+  # set output to write as "violations_chart_<datetime>.pdf"
+  fname <- paste("violations_chart_",as.character(Sys.time()),".pdf")
+  pdf(fname)
+  
+  plot <- ggplot(data = landlords) +
+    aes(y=total_viol, x = landlord_name) +
+    geom_col() +
+    coord_flip() +
+    scale_y_continuous(labels = scales::comma) +
+    theme_minimal() +
+    theme(
+      panel.grid.major.y = element_blank(),
+      axis.text.y = element_text(margin=margin(r=-15)),
+      plot.caption = element_text(face="italic",color="darkgrey",margin=margin(t=10))) +
+    labs(
+      title = '10 "Worst" Landlords in NYC',
+      subtitle = 'Total HPD Violations',
+      x=NULL,
+      y="Number of Violations",
+      caption = "Source: watchlist"
+    )
+  
+  print(plot)
+  dev.off()
+  
+}
+  
 
 
 
