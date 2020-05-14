56bd87da8ac1abe4acf8f505e34a611c0aa62879
hpsleuth7 <murphy.c.austin@gmail.com>
2020-05-14 16:05:26 -0400

    finalized DOC admit analysis function, needs debugging

--- doc/admit_analysis.R
+++ doc/admit_analysis.R
@@ -1,5 +1,30 @@
+### Admit Analysis
+### Usage: function reads CSV in local data directory 
+###        Visualizes smoothed curve of DOC admissions in 2020
+library("tidyverse")
+library("quantreg")
+
+main <- function() {
+
+  admits <- read.csv("data/Inmate_Admissions_2020-05-10.csv")
+
+  admits$admitted_dt <- as.Date(substr(levels(admits$admitted_dt)[admits$admitted_dt],1,10),format='%m/%d/%Y')
 
 
-admits <- read.csv("data/Inmate_Admissions_2020-05-10.csv")
+  recent <- mutate(admits, admitted_dt= 
+		as.Date(substr(levels(admitted_dt)[admitted_dt],1,10),
+		format='%m/%d/%Y')) %>%
+    filter(admitted_dt >= as.Date("2020/1/1")) %>%
+    group_by(admitted_dt) %>%
+    summarize(total_admits = n())
 
-as.Date(substr(levels(admits$admitted_dt)[admits$admitted_dt[1:10]],1,10),format='%m/%d/%Y')
+  ggplot(data=recent) +
+    aes(x=admitted_dt,y=total_admits) +
+    geom_smooth()
+  
+}
+
+main()
+
+
+
