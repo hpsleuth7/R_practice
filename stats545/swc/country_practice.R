d6d6d17d1ec51eb3503e73d82d91fd14a84d00c9
hpsleuth7 <murphy.c.austin@gmail.com>
2020-06-24 16:44:38 -0400

    practice w aggregators and dplyr

--- stats545/swc/country_practice.R
+++ stats545/swc/country_practice.R
@@ -3,7 +3,23 @@ library(gapminder)
 
 my_gap <- gapminder
 
-my_gap %>% 
+continent_life_exp = my_gap %>% 
   group_by(continent) %>% 
-  summarize(avg_life = mean(lifeExp)
+  summarize(avg_life = mean(lifeExp))
 
+print(continent_life_exp)
+
+continent_life_gdp = my_gap %>% 
+  filter(year %in% c(1952,2007)) %>% 
+  group_by(continent,year) %>% 
+  summarize_at(vars(lifeExp,gdpPercap),list(~median(.),~mean(.)))
+
+print(continent_life_gdp)
+
+asia_extreme_life = my_gap %>% 
+  filter(continent=='Asia') %>% 
+  group_by(year) %>%
+  summarize(min_lifeExp=min(lifeExp),max_lifeExp=max(lifeExp))
+
+print(asia_extreme_life)
+  
