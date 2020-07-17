da340b9af4e34fcae262808ea416fc2c108d8c7b
hpsleuth7 <murphy.c.austin@gmail.com>
2020-07-17 16:10:10 -0400

    tidying script for lotr words

--- stats545/swc/lotr_tidy_practice.R
+++ stats545/swc/lotr_tidy_practice.R
@@ -2,3 +2,13 @@
 # From exercises https://github.com/jennybc/lotr-tidy#readme
 
 library(tidyverse)
+
+#read data
+fship <- read_csv("data/The_Fellowship_Of_The_Ring.csv")
+ttow <- read_csv("data/The_Two_Towers.csv")
+retk <- read_csv("data/The_Return_Of_The_King.csv")
+
+lotr_words <- bind_rows(fship,ttow,retk)  #combine into one dat
+
+lotr_words <- lotr_words %>%    #gather wordcount into column by gender
+  gather(key="Gender",value="Words",Female,Male)
